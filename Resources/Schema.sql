bb62fdcf97f0015b2db26d0fc6255c1a190ba81f
Adrian Jude Gallyot <83386111+AdrianGallyot@users.noreply.github.com>
2021-09-04 22:05:04 +0800

    Add files via upload

---
+++ Resources/Schema.sql
@@ -0,0 +1,201 @@
+CREATE TABLE Cities
+(
+    index bigint,
+    City text
+)
+
+CREATE TABLE CityTemp
+(
+    index bigint,
+    Region text ,
+    Country text,
+    City text,
+    Month bigint,
+    Day bigint,
+    Year bigint,
+    AvgTemperature double precision
+)
+
+CREATE TABLE Countries
+(
+    index bigint,
+    Country text
+)
+
+CREATE TABLE CurrentCityWeather
+(
+    index bigint,
+    city text,
+    lat double precision,
+    temp double precision
+)
+
+CREATE TABLE Element
+(
+    Element Code bigint,
+    Element text
+)
+
+CREATE TABLE EnvArea
+(
+    Area Code bigint,
+    Area text
+)
+
+CREATE TABLE Month
+(
+    Months Code bigint,
+    Months text
+)
+
+CREATE TABLE Regions
+(
+    index bigint,
+    Region text
+)
+
+CREATE TABLE public.Std_Dev
+(
+    index bigint,
+    Area Code bigint,
+    Area text,
+    Months Code bigint,
+    Months text,
+    Element Code bigint,
+    Element text,
+    Y1961 double precision,
+    Y1962 double precision,
+    Y1963 double precision,
+    Y1964 double precision,
+    Y1965 double precision,
+    Y1966 double precision,
+    Y1967 double precision,
+    Y1968 double precision,
+    Y1969 double precision,
+    Y1970 double precision,
+    Y1971 double precision,
+    Y1972 double precision,
+    Y1973 double precision,
+    Y1974 double precision,
+    Y1975 double precision,
+    Y1976 double precision,
+    Y1977 double precision,
+    Y1978 double precision,
+    Y1979 double precision,
+    Y1980 double precision,
+    Y1981 double precision,
+    Y1982 double precision,
+    Y1983 double precision,
+    Y1984 double precision,
+    Y1985 double precision,
+    Y1986 double precision,
+    Y1987 double precision,
+    Y1988 double precision,
+    Y1989 double precision,
+    Y1990 double precision,
+    Y1991 double precision,
+    Y1992 double precision,
+    Y1993 double precision,
+    Y1994 double precision,
+    Y1995 double precision,
+    Y1996 double precision,
+    Y1997 double precision,
+    Y1998 double precision,
+    Y1999 double precision,
+    Y2000 double precision,
+    Y2001 double precision,
+    Y2002 double precision,
+    Y2003 double precision,
+    Y2004 double precision,
+    Y2005 double precision,
+    Y2006 double precision,
+    Y2007 double precision,
+    Y2008 double precision,
+    Y2009 double precision,
+    Y2010 double precision,
+    Y2011 double precision,
+    Y2012 double precision,
+    Y2013 double precision,
+    Y2014 double precision,
+    Y2015 double precision,
+    Y2016 double precision,
+    Y2017 double precision,
+    Y2018 double precision,
+    Y2019 double precision
+)
+
+CREATE TABLE public.TempVar
+(
+    index bigint,
+    Area Code bigint,
+    Area text,
+    Months Code bigint,
+    Months text,
+    Element Code bigint,
+    Element text,
+    Y1961 double precision,
+    Y1962 double precision,
+    Y1963 double precision,
+    Y1964 double precision,
+    Y1965 double precision,
+    Y1966 double precision,
+    Y1967 double precision,
+    Y1968 double precision,
+    Y1969 double precision,
+    Y1970 double precision,
+    Y1971 double precision,
+    Y1972 double precision,
+    Y1973 double precision,
+    Y1974 double precision,
+    Y1975 double precision,
+    Y1976 double precision,
+    Y1977 double precision,
+    Y1978 double precision,
+    Y1979 double precision,
+    Y1980 double precision,
+    Y1981 double precision,
+    Y1982 double precision,
+    Y1983 double precision,
+    Y1984 double precision,
+    Y1985 double precision,
+    Y1986 double precision,
+    Y1987 double precision,
+    Y1988 double precision,
+    Y1989 double precision,
+    Y1990 double precision,
+    Y1991 double precision,
+    Y1992 double precision,
+    Y1993 double precision,
+    Y1994 double precision,
+    Y1995 double precision,
+    Y1996 double precision,
+    Y1997 double precision,
+    Y1998 double precision,
+    Y1999 double precision,
+    Y2000 double precision,
+    Y2001 double precision,
+    Y2002 double precision,
+    Y2003 double precision,
+    Y2004 double precision,
+    Y2005 double precision,
+    Y2006 double precision,
+    Y2007 double precision,
+    Y2008 double precision,
+    Y2009 double precision,
+    Y2010 double precision,
+    Y2011 double precision,
+    Y2012 double precision,
+    Y2013 double precision,
+    Y2014 double precision,
+    Y2015 double precision,
+    Y2016 double precision,
+    Y2017 double precision,
+    Y2018 double precision,
+    Y2019 double precision
+)
+
+
+
+
+
+
---
+++ Resources/TemperatureDB.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE TemperatureDB
+   WITH OWNER postgres 
+   TEMPLATE template0
+   ENCODING 'UTF-8'
+   TABLESPACE  pg_default
+   LC_COLLATE  'C'
+   LC_CTYPE  'C'
+   CONNECTION LIMIT  -1;
